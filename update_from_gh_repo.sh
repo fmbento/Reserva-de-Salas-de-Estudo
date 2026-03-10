24a95d494fae848be99a72393a23a0bace962484
Filipe Bento <fmbento@users.noreply.github.com>
2026-03-10 17:23:54 +0000

    feat: Automate deployment script
    Adds a script to automatically pull changes from the GitHub repository, rebuild Docker images, stop existing containers, and start new ones. This streamlines the deployment process.

---
+++ update_from_gh_repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git pull
+# Coloca este script de atualização do container docker a partir do repositorio>
+chmod +x ./update_from_gh_repo.sh
+sudo docker-compose build
+sudo docker-compose down
+sudo docker-compose up -d
